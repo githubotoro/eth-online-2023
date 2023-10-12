36e94639333f0671cfd758e6ccd3f8e5dbf41ff8
githubotoro <yupuday.git@gmail.com>
2023-10-11 19:29:11 -0700

    integrated cometh wallet creation

---
+++ next-app/app/login/page.jsx
@@ -0,0 +1,13 @@
+import { ComethConnector } from "./ComethConnector";
+
+const LoginPage = () => {
+	return (
+		<div>
+			Login Page
+			<hr />
+			<ComethConnector />
+		</div>
+	);
+};
+
+export default LoginPage;
